bb42ee24fa3a394f805e442aae9e1f84f30a40a2
Marcela Mazo <marcela.mazo@correounivalle.edu.co>
2024-04-24 23:57:09 -0500

    Fix the navbar

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Navbar from "./components/Navbar";
+import Navbar from './components/Navbar';
 import BpmnView from "./diagramViewer";
 import ModelerCreator from "./diagramCreator/index";
 import "./styles.css";
