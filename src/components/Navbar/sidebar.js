85e7e5f5f57fc841ef47d9e9d42940ff365ed8c2
edinsonuwu <edinson.dorado@correounivalle.edu.co>
2024-05-31 15:18:30 -0500

    iconos de edicion y borrar

--- src/components/Navbar/sidebar.js
+++ src/components/Navbar/sidebar.js
@@ -1,30 +1,49 @@
 import React, { useState, useEffect } from "react";
+import PropTypes from 'prop-types';
 import { Drawer, Menu } from 'antd';
+import { EditOutlined, DeleteOutlined } from '@ant-design/icons';
 import { useTranslation } from 'react-i18next';
 import './sidebar.css';
 import logo from '../../images/logo.png';
-import {useDiagramDefinitions} from "../../contexts/DiagramDefinitions";
+import { useDiagramDefinitions } from "../../contexts/DiagramDefinitions";
+
+// Helper function to fetch BPMN List from localStorage
+const fetchBpmnListFromStorage = () => {
+    console.log("Fetching BPMN List from localStorage");
+    const storedBpmnList = localStorage.getItem('bpmnList');
+    return storedBpmnList ? JSON.parse(storedBpmnList) : [];
+};
 
 function Sidebar({ visible, onClose }) {
     const [t] = useTranslation("global");
-    const [bpmnList, setBpmnList] = useState([]); // Estado para almacenar los elementos de bpmnList
-    const { setDiagramDefinitions} = useDiagramDefinitions();
-    // Función para obtener los elementos de localStorage y parsearlos
-    const fetchBpmnList = () => {
-        console.log("Fetching BPMN List from localStorage");
-        const storedBpmnList = localStorage.getItem('bpmnList');
-        if (storedBpmnList) {
-            setBpmnList(JSON.parse(storedBpmnList));
-        }
-    };
+    const [bpmnList, setBpmnList] = useState([]);
+    const { setDiagramDefinitions } = useDiagramDefinitions();
+    const [selectedKey, setSelectedKey] = useState(null); // Estado para rastrear el elemento seleccionado
+
 
     useEffect(() => {
-        fetchBpmnList(); // Llamar a la función cuando el componente se monte
+        setBpmnList(fetchBpmnListFromStorage());
     }, []);
 
-    // Función que se llama al abrir el Drawer
     const handleOnOpen = () => {
-        fetchBpmnList(); // Actualizar la lista cuando se abre el Drawer
+        setBpmnList(fetchBpmnListFromStorage());
+    };
+
+    const handleMenuItemClick = (item, index) => {
+        setSelectedKey(index); // Establecer el índice del elemento seleccionado
+        console.log(setDiagramDefinitions);
+        console.log('Menu item clicked:', item);
+        //logica de si se presiono algun icono, no pase lo de arriba
+    };
+
+    const handleEditClick = (item) => {
+        console.log('Edit item clicked:', item);
+        // Aquí puedes agregar la lógica para editar el item
+    };
+
+    const handleDeleteClick = (item) => {
+        console.log('Delete item clicked:', item);
+        // Aquí puedes agregar la lógica para eliminar el item
     };
 
     return (
@@ -39,7 +58,7 @@ function Sidebar({ visible, onClose }) {
             placement="left"
             closable={true}
             onClose={onClose}
-            afterOpenChange ={(visible) => {
+            afterOpenChange={(visible) => {
                 if (visible) {
                     handleOnOpen();
                 }
@@ -48,13 +67,35 @@ function Sidebar({ visible, onClose }) {
         >
             <Menu>
                 {bpmnList.map((item, index) => (
-                    <Menu.Item key={index} onClick={() => {console.log(setDiagramDefinitions);console.log('Menu item clicked:', item)}}>
-                        {item.title}
+                    <Menu.Item 
+                        key={index}
+                        style={{
+                            backgroundColor: selectedKey === index ? '#BDBDBD' : '#dbdee0',
+                            color: selectedKey === index ? 'black' : 'black'
+                        }}
+                        onClick={() => handleMenuItemClick(item, index)}
+                        >
+                        <span >{item.title}</span>
+                        <DeleteOutlined 
+                            className="icon"
+
+                            onClick={() => handleDeleteClick(item)} 
+                        />
+                        <EditOutlined 
+                            className="icon"
+
+                            onClick={() => handleEditClick(item)} 
+                        />
                     </Menu.Item>
                 ))}
             </Menu>
         </Drawer>
     );
 }
+
+Sidebar.propTypes = {
+    visible: PropTypes.bool.isRequired,
+    onClose: PropTypes.func.isRequired
+};
 
 export default Sidebar;
