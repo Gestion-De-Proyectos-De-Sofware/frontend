c798c53e92b81d11aece13f6b8fa56ff97c53e87
@Bluruwu <juandiegotres@hotmail.com>
2024-06-05 20:16:51 -0500

    add new tests

--- cypress/e2e/pruebas.cy.js
+++ cypress/e2e/pruebas.cy.js
@@ -56,6 +56,83 @@ describe('ModelerCreator Component', () => {
         // Verifica que el diagrama se haya reiniciado cargando el diagramXML
         cy.get('.djs-container').should('exist'); 
     })
-    
+
+    it('BPM Storage', () => {
+      cy.get('#js-canvas').should('exist'); // Verifica que el contenedor del lienzo exista
+      cy.get('.navbar-buttons > :nth-child(1)').click(); // Click en el archivo de la navbar
+      cy.get('#saveItem > .ant-dropdown-menu-title-content').click(); // Click en el botón guardar de la navbar
+      cy.get('.swal2-popup').should('be.visible'); //Verifica que el popup de nombre salga en pantalla
+      cy.get('#swal2-input').type('New'); //Escribe el nombre del BPM
+      cy.get('#swal2-input').should('have.value', 'New'); //Verifica que el input contiene el texto
+      cy.get('.swal2-confirm').click(); //Guarda el BPM
+      cy.get('.swal2-confirm').click(); //Acepta confirmacion de guardado
+      cy.get('.ant-btn-icon-only').click(); //Click sidebar
+      cy.get('.ant-menu-item').contains('New').should('exist');
+    })
+
+    it('Cookie in localStorage', () => {
+      cy.get('#js-canvas').should('exist'); // Verifica que el contenedor del lienzo exista
+      cy.get('.navbar-buttons > :nth-child(1)').click(); // Click en el archivo de la navbar
+      cy.get('#saveItem > .ant-dropdown-menu-title-content').click(); // Click en el botón guardar de la navbar
+      cy.get('.swal2-popup').should('be.visible'); //Verifica que el popup de nombre salga en pantalla
+      cy.get('#swal2-input').type('New'); //Escribe el nombre del BPM
+      cy.get('#swal2-input').should('have.value', 'New'); //Verifica que el input contiene el texto
+      cy.get('.swal2-confirm').click(); //Guarda el BPM
+      cy.get('.swal2-confirm').click();
+      // Accede a la ventana global y verifica el valor en localStorage
+      cy.window().then((window) => {
+        const bpmnList = window.localStorage.getItem('bpmnList');
+        expect(bpmnList).to.not.be.null;
+        // Parsear el JSON almacenado en localStorage
+        const bpmnArray = JSON.parse(bpmnList);
+        const hasCorrectTitle = bpmnArray.some(item => item.title === 'New');
+        expect(hasCorrectTitle).to.be.true;
+      });
+    });
+
+    it('Edited title working', () => {
+      cy.get('#js-canvas').should('exist'); // Verifica que el contenedor del lienzo exista
+      cy.get('.navbar-buttons > :nth-child(1)').click(); // Click en el archivo de la navbar
+      cy.get('#saveItem > .ant-dropdown-menu-title-content').click(); // Click en el botón guardar de la navbar
+      cy.get('.swal2-popup').should('be.visible'); //Verifica que el popup de nombre salga en pantalla
+      cy.get('#swal2-input').type('New'); //Escribe el nombre del BPM
+      cy.get('#swal2-input').should('have.value', 'New'); //Verifica que el input contiene el texto
+      cy.get('.swal2-confirm').click(); //Guarda el BPM
+      cy.get('.swal2-confirm').click(); //Acepta confirmacion de guardado
+      cy.get('.ant-btn-icon-only').click(); //Click sidebar  
+      cy.get('.anticon.anticon-edit.icon').click(); //Click sidebar  
+      cy.get('.ant-input').clear().type('New 2 {enter}');
+      cy.get('.ant-menu-item').contains('New 2').should('exist')
+    });
+
+    it('Edited title working in LocalStorage', () => {
+      cy.get('#js-canvas').should('exist'); // Verifica que el contenedor del lienzo exista
+      cy.get('.navbar-buttons > :nth-child(1)').click(); // Click en el archivo de la navbar
+      cy.get('#saveItem > .ant-dropdown-menu-title-content').click(); // Click en el botón guardar de la navbar
+      cy.get('.swal2-popup').should('be.visible'); //Verifica que el popup de nombre salga en pantalla
+      cy.get('#swal2-input').type('New'); //Escribe el nombre del BPM
+      cy.get('#swal2-input').should('have.value', 'New'); //Verifica que el input contiene el texto
+      cy.get('.swal2-confirm').click(); //Guarda el BPM
+      cy.get('.swal2-confirm').click(); //Acepta confirmacion de guardado
+      cy.window().then((window) => {
+        const bpmnList = window.localStorage.getItem('bpmnList');
+        expect(bpmnList).to.not.be.null;
+        // Parsear el JSON almacenado en localStorage
+        const bpmnArray = JSON.parse(bpmnList);
+        const hasCorrectTitle = bpmnArray.some(item => item.title === 'New');
+        expect(hasCorrectTitle).to.be.true;
+      });
+      cy.get('.ant-btn-icon-only').click(); //Click sidebar  
+      cy.get('.anticon.anticon-edit.icon').click(); //Click sidebar  
+      cy.get('.ant-input').clear().type('New 2{enter}');
+      cy.window().then((window) => {
+        const bpmnList = window.localStorage.getItem('bpmnList');
+        expect(bpmnList).to.not.be.null;
+        // Parsear el JSON almacenado en localStorage
+        const bpmnArray = JSON.parse(bpmnList);
+        const hasCorrectTitle = bpmnArray.some(item => item.title === 'New 2');
+        expect(hasCorrectTitle).to.be.true;
+      });
+    });
   })
   
